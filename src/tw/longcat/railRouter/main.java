57b431cac9079d297c6290bd5465ddb7f0af9fa0
catLee <leemiyinghao@gmail.com>
2014-03-29 17:16:23 +0900

    Basic needs of a bukkit plugin

---
+++ src/tw/longcat/railRouter/main.java
@@ -0,0 +1,9 @@
+package tw.longcat.railRouter;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class main extends JavaPlugin{
+	public void onEnable(){
+		this.getServer().broadcast("Hello railRouter!", "*");
+	}
+}
